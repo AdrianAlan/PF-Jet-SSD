b2cf637a004c3d24711c3b19ceb71c915fa9970b
AdrianAlan <adrianalan.pol@gmail.com>
2020-02-14 13:36:44 +0100

    Add validation set

--- pytorch-train.py
+++ pytorch-train.py
@@ -45,11 +45,13 @@ if torch.cuda.is_available():
 else:
     torch.set_default_tensor_type('torch.FloatTensor')
 
+
 HOME = pathlib.Path().absolute()
 DATA_SOURCE = '/eos/user/a/adpol/ceva/fast'
 
 dataset = 'ssd-jet-tests'
 train_dataset_path = '%s/RSGraviton_NARROW_0.h5' % DATA_SOURCE
+val_dataset_path = '%s/RSGraviton_NARROW_1.h5' % DATA_SOURCE
 save_folder = os.path.join(HOME, 'models/')
 
 # Learning Parameters
@@ -75,6 +77,15 @@ train_loader = torch.utils.data.DataLoader(train_dataset,
                                            collate_fn=detection_collate,
                                            shuffle=True,
                                            num_workers=num_workers)
+
+h5_val = h5py.File(val_dataset_path, 'r')
+val_dataset = CalorimeterJetDataset(hdf5_dataset=h5_val)
+val_loader = torch.utils.data.DataLoader(val_dataset,
+                                         batch_size=batch_size,
+                                         collate_fn=detection_collate,
+                                         shuffle=True,
+                                         num_workers=num_workers)
+
 
 # Build SSD Network
 ssd_net = build_ssd('train', 300, num_classes + 1, False)
@@ -116,7 +127,7 @@ for epoch in range(1, train_epochs+1):
     if epoch in lr_steps:
         adjust_learning_rate(optimizer, gamma, lr)
 
-    batch_loss_l, batch_loss_c = np.empty(0), np.empty(0)
+    train_loss_l, train_loss_c = np.empty(0), np.empty(0)
 
     tr = trange(len(train_loader)*batch_size, file=sys.stdout)
     tr.set_description('Epoch {}'.format(epoch))
@@ -129,27 +140,48 @@ for epoch in range(1, train_epochs+1):
         optimizer.zero_grad()
         output = net(images)
         loss_l, loss_c = criterion(output, targets)
-        batch_loss_l = np.append(batch_loss_l, loss_l.item())
-        batch_loss_c = np.append(batch_loss_c, loss_c.item())
+        train_loss_l = np.append(train_loss_l, loss_l.item())
+        train_loss_c = np.append(train_loss_c, loss_c.item())
         loss = loss_l + loss_c
         loss.backward()
         optimizer.step()
-        
-        av_batch_loss_l = np.average(batch_loss_l)
-        av_batch_loss_c = np.average(batch_loss_c)
 
-        if epoch < 10:
-            tr.set_description(
-                 'Epoch {} Loss {:.5f} Localization {:.5f} Classification {:.5f}'.format(
-                     epoch, av_batch_loss_l + av_batch_loss_c,
-                     av_batch_loss_l, av_batch_loss_c))
-        else:
-            tr.set_description(
-                 'Epoch {} Loss {:.5f} Localization {:.5f} Classification {:.5f}'.format(
-                     epoch, av_batch_loss_l + av_batch_loss_c,
-                     av_batch_loss_l, av_batch_loss_c))
+        av_batch_loss_l = np.average(train_loss_l)
+        av_batch_loss_c = np.average(train_loss_c)
+
+        tr.set_description(
+            'Epoch {} Loss {:.5f} Localization {:.5f} Classification {:.5f}'.format(
+                epoch, av_batch_loss_l + av_batch_loss_c,
+                av_batch_loss_l, av_batch_loss_c))
 
         tr.update(len(images))
+
+    tr.close()
+
+    # Calculate validation loss
+    val_loss_l, val_loss_c = np.empty(0), np.empty(0)
+
+    tr = trange(len(val_loader)*batch_size, file=sys.stdout)
+    tr.set_description('Validation')
+
+    with torch.no_grad():
+        for batch_index, (images, targets) in enumerate(val_loader):
+            images = Variable(images.cuda())
+            targets = [Variable(ann.cuda()) for ann in targets]
+            output = net(images)
+            loss_l, loss_c = criterion(output, targets)
+            val_loss_l = np.append(val_loss_l, loss_l.item())
+            val_loss_c = np.append(val_loss_c, loss_c.item())
+   
+            av_batch_loss_l = np.average(val_loss_l)
+            av_batch_loss_c = np.average(val_loss_c)
+
+            tr.set_description(
+                'Validation Loss {:.5f} Localization {:.5f} Classification {:.5f}'.format(
+                    av_batch_loss_l + av_batch_loss_c,
+                    av_batch_loss_l, av_batch_loss_c))
+
+            tr.update(len(images))
 
     tr.close()
 
