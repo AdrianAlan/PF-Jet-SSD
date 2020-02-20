52c6703db93131bf7c1e45d66d971440fca67e28
AdrianAlan <adrianalan.pol@gmail.com>
2020-02-20 18:30:39 +0100

    Rewrite scoring function

--- pytorch-eval.py
+++ pytorch-eval.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 from ssd.generator import CalorimeterJetDataset
 from ssd.net import build_ssd
+from sklearn.metrics import average_precision_score
 import h5py
 import numpy as np
 import torch
@@ -11,27 +12,10 @@ import torch.utils.data as data
 import sys
 
 
-def voc_ap(rec, prec):
-    mrec = np.concatenate(([0.], rec, [1.]))
-    mpre = np.concatenate(([0.], prec, [0.]))
-
-    # compute the precision envelope
-    for i in range(mpre.size - 1, 0, -1):
-        mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
-
-    # to calculate area under PR curve, look for points
-    # where X axis (recall) changes value
-    i = np.where(mrec[1:] != mrec[:-1])[0]
-
-    ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
-    return ap
-
-
 def test_net(model, dataset, top_k, im_size=(300, 300),
              conf_threshold=0.05, overlap_threshold=0.1):
 
-    results = np.empty((0, 3))
-    results2 = np.empty((0, 3))
+    results = np.empty((0, 2))
 
     with torch.no_grad():
         for data, targets in dataset:
@@ -44,7 +28,7 @@ def test_net(model, dataset, top_k, im_size=(300, 300),
             targets[:, 1] *= im_size[1]
             targets[:, 3] *= im_size[1]
 
-            agg_detections = np.empty((0, 6))
+            all_detections = np.empty((0, 7))
             for j in range(1, detections.size(1)):
                 dets = detections[0, j, :]
 
@@ -65,87 +49,51 @@ def test_net(model, dataset, top_k, im_size=(300, 300),
 
                 class_det = np.hstack((boxes.cpu().numpy(),
                                        labels[:, np.newaxis],
-                                       scores[:, np.newaxis])
+                                       scores[:, np.newaxis],
+                                       np.zeros(len(boxes))[:, np.newaxis])
                                       ).astype(np.float32, copy=False)
 
-                agg_detections = np.vstack((agg_detections, class_det))
+                all_detections = np.vstack((all_detections, class_det))
 
             # Sort by confidence
-            agg_detections = agg_detections[(-agg_detections[:, -1]).argsort()]
+            all_detections = all_detections[(-all_detections[:, -1]).argsort()]
 
             # Select top k predictions
-            agg_detections = agg_detections[:top_k]
+            all_detections = all_detections[:top_k]
 
-            # Loop over agg_detections
-            for d in agg_detections:
-
-                detected = 0
-                # Check the overlap
-                ixmin = np.maximum(targets[:, 0], d[0])
-                iymin = np.maximum(targets[:, 1], d[1])
-                ixmax = np.minimum(targets[:, 2], d[2])
-                iymax = np.minimum(targets[:, 3], d[3])
-
-                iw = np.maximum(ixmax - ixmin, 0.)
-                ih = np.maximum(iymax - iymin, 0.)
-
-                inters = iw * ih
-                uni = ((d[2] - d[0]) * (d[3] - d[1]) +
-                       (targets[:, 2] - targets[:, 0]) *
-                       (targets[:, 3] - targets[:, 1]) - inters) + 10e-12
-
-                overlaps = inters / uni
-                overlaps = np.max(overlaps)
-
-                if overlaps > overlap_threshold:
-                    detected = 1
-
-                prediction = np.array([d[-1], detected, int(not detected)])
-                results = np.vstack((results, prediction))
-
-            # Loop over agg_detections
+            # Detection format: [xmin, ymin, xmax, ymax, label, score, tp/fp]
+            # Loop over detections
             for t in targets:
+                detected = False
 
-                detected = 0
-                # Check the overlap
-                ixmin = np.maximum(agg_detections[:, 0], t[0])
-                iymin = np.maximum(agg_detections[:, 1], t[1])
-                ixmax = np.minimum(agg_detections[:, 2], t[2])
-                iymax = np.minimum(agg_detections[:, 3], t[3])
+                for x, d in enumerate(all_detections):
+                    ixmin = np.maximum(t[0], d[0])
+                    iymin = np.maximum(t[1], d[1])
+                    ixmax = np.minimum(t[2], d[2])
+                    iymax = np.minimum(t[3], d[3])
 
-                iw = np.maximum(ixmax - ixmin, 0.)
-                ih = np.maximum(iymax - iymin, 0.)
+                    iw = np.maximum(ixmax - ixmin, 0.)
+                    ih = np.maximum(iymax - iymin, 0.)
+                    intersection = iw * ih
 
-                inters = iw * ih
-                uni = ((t[2] - t[0]) * (t[3] - t[1]) +
-                       (agg_detections[:, 2] - agg_detections[:, 0]) *
-                       (agg_detections[:, 3] - agg_detections[:, 1]) -
-                       inters) + 10e-12
+                    union = ((d[2] - d[0]) * (d[3] - d[1]) +
+                             (t[2] - t[0]) * (t[3] - t[1]) - intersection)
 
-                overlaps = inters / uni
-                overlaps_x = overlaps.argmax()
-                overlaps = np.max(overlaps)
+                    overlap = intersection / (union + 10e-12)
 
-                if overlaps > overlap_threshold:
-                    detected = 1
+                    if overlap > overlap_threshold:
+                        detected = True
+                        all_detections[x][6] = 1
+                        break
 
-                prediction = np.array([agg_detections[overlaps_x, -1],
-                                       detected, int(not detected)])
-                results2 = np.vstack((results2, prediction))
+                if detected:
+                    fn = np.hstack((np.zeros(6), [1])).astype(np.float32,
+                                                              copy=False)
+                    all_detections = np.vstack((all_detections, fn))
 
-        results = results[(-results[:, 0]).argsort()]
-        fp = np.cumsum(results[:, 2])
-        tp = np.cumsum(results[:, 1])
-        prec = tp / (tp + fp)
+                results = np.vstack((results, all_detections[:, [6, 5]]))
 
-        results2 = results2[(-results2[:, 0]).argsort()]
-        fn = np.cumsum(results2[:, 2])
-        tp = np.cumsum(results2[:, 1])
-        rec = tp / (tp + fn)
-        print(rec)
-        print(prec)
-        ap = voc_ap(rec, prec)
-        return ap
+        return average_precision_score(results[:, 0], results[:, 1])
 
 
 if __name__ == '__main__':
@@ -155,7 +103,7 @@ if __name__ == '__main__':
     else:
         torch.set_default_tensor_type('torch.FloatTensor')
 
-    model_source_path = './models/ssd-jet-2.pth'
+    model_source_path = './models/ssd-jet-tests.pth'
 
     num_classes = 1
     num_classes = num_classes + 1  # +1 for background
@@ -175,12 +123,7 @@ if __name__ == '__main__':
                                                shuffle=False,
                                                num_workers=1)
 
-    top_k = 8
-    confidence_threshold = 0.01
-    overlap_threshold = 0.25
-    img_size = (300, 300)
-
     for i in range(1, num_classes):
-        ap = test_net(net, train_loader, top_k, img_size,
-                      confidence_threshold, overlap_threshold)
+        ap = test_net(net, train_loader, top_k=10, im_size=(300, 300),
+                      conf_threshold=0.01, overlap_threshold=0.5)
         print('Average precision for class %s: %s' % (i, ap))
