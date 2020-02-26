66dfd99b347bb396c8370896c06d9b4409ed5f6a
AdrianAlan <adrianalan.pol@gmail.com>
2020-02-26 19:17:03 +0100

    Change bbox default sizes

--- ssd/layers/functions/prior_box.py
+++ ssd/layers/functions/prior_box.py
@@ -25,21 +25,16 @@ class PriorBox(Function):
         for k, f in enumerate(feature_maps):
             for i, j in product(range(f), repeat=2):
                 f_k = image_size / steps[k]
-                # unit center x,y
+
                 cx = (j + 0.5) / f_k
                 cy = (i + 0.5) / f_k
 
-                # aspect_ratio: 1
-                # rel size: min_size
-                s_k = min_sizes[k]/image_size
-                mean += [cx, cy, s_k, s_k]
+                s_k_min = min_sizes[k]/image_size
+                mean += [cx, cy, s_k_min, s_k_min]
 
-                # aspect_ratio: 1
-                # rel size: sqrt(s_k * s_(k+1))
-                s_k_prime = sqrt(s_k * (max_sizes[k]/image_size))
-                mean += [cx, cy, s_k_prime, s_k_prime]
+                s_k_max = max_sizes[k]/image_size
+                mean += [cx, cy, s_k_max, s_k_max]
 
-        # back to torch land
         output = torch.Tensor(mean).view(-1, 4)
         if clip:
             output.clamp_(max=1, min=0)
