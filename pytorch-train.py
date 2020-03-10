6f1fa13c61f1d729f01035488c5ab61014809930
AdrianAlan <adrianalan.pol@gmail.com>
2020-03-10 09:38:04 -0400

    Restore validation in BNNs

--- pytorch-train.py
+++ pytorch-train.py
@@ -53,9 +53,9 @@ else:
 
 HOME = pathlib.Path().absolute()
 DATA_SOURCE = '/eos/user/a/adpol/ceva/fast'
-BINARY = False
+BINARY = True
 
-dataset = 'ssd-jet-tests'
+dataset = 'ssd-jet-binary'
 train_dataset_path = '%s/RSGraviton_NARROW_0.h5' % DATA_SOURCE
 val_dataset_path = '%s/RSGraviton_NARROW_1.h5' % DATA_SOURCE
 save_folder = os.path.join(HOME, 'models/')
@@ -64,10 +64,10 @@ save_folder = os.path.join(HOME, 'models/')
 num_workers = 1
 num_classes = 1
 batch_size = 50
-train_epochs = 250
+train_epochs = 10
 
 if BINARY:
-    lr = 1e-2
+    lr = 1e-3
     lrs = [5e-3, 1e-3, 5e-4, 1e-4]
     momentum = 0.9
     lr_steps = [10, 15, 20, 25]
@@ -107,7 +107,7 @@ ssd_net = build_ssd('train', 300, num_classes + 1, BINARY)
 net = torch.nn.DataParallel(ssd_net)
 cudnn.benchmark = True
 net = net.cuda()
-
+print(ssd_net)
 # Initialize Weights
 ssd_net.vgg.apply(weights_init)
 ssd_net.extras.apply(weights_init)
@@ -193,51 +193,45 @@ for epoch in range(1, train_epochs+1):
 
     tr.close()
 
-    # Validation works only on full precision network
-    if not BINARY:
+    tr = trange(len(val_loader)*batch_size, file=sys.stdout)
+    tr.set_description('Validation')
+    batch_loss_l, batch_loss_c = np.empty(0), np.empty(0)
+    net.eval()
 
-        tr = trange(len(val_loader)*batch_size, file=sys.stdout)
-        tr.set_description('Validation')
-        batch_loss_l, batch_loss_c = np.empty(0), np.empty(0)
-        net.eval()
+    with torch.no_grad():
+        for batch_index, (images, targets) in enumerate(val_loader):
 
-        with torch.no_grad():
-            for batch_index, (images, targets) in enumerate(val_loader):
+            images = Variable(images.cuda())
+            targets = [Variable(ann.cuda()) for ann in targets]
 
-                images = Variable(images.cuda())
-                targets = [Variable(ann.cuda()) for ann in targets]
+            output = net(images)
+            loss_l, loss_c = criterion(output, targets)
+            batch_loss_l = np.append(batch_loss_l, loss_l.item())
+            batch_loss_c = np.append(batch_loss_c, loss_c.item())
 
-                output = ssd_net(images)
-                loss_l, loss_c = criterion(output, targets)
-                batch_loss_l = np.append(batch_loss_l, loss_l.item())
-                batch_loss_c = np.append(batch_loss_c, loss_c.item())
+            av_val_loss_l = np.average(batch_loss_l)
+            av_val_loss_c = np.average(batch_loss_c)
+            av_val_loss = av_val_loss_l + av_val_loss_c
 
-                av_val_loss_l = np.average(batch_loss_l)
-                av_val_loss_c = np.average(batch_loss_c)
-                av_val_loss = av_val_loss_l + av_val_loss_c
+            tr.set_description(
+                ('Validation Loss {:.5f} ' +
+                 'Localization {:.5f} Classification {:.5f}').format(
+                   av_val_loss, av_val_loss_l, av_val_loss_c))
 
-                tr.set_description(
-                    ('Validation Loss {:.5f} ' +
-                     'Localization {:.5f} Classification {:.5f}').format(
-                       av_val_loss, av_val_loss_l, av_val_loss_c))
+            tr.update(len(images))
 
-                tr.update(len(images))
+    tr.close()
 
-        tr.close()
+    val_loss_l = np.append(val_loss_l, av_val_loss_l)
+    val_loss_c = np.append(val_loss_c, av_val_loss_c)
+    val_loss = np.append(val_loss, av_val_loss)
 
-        val_loss_l = np.append(val_loss_l, av_val_loss_l)
-        val_loss_c = np.append(val_loss_c, av_val_loss_c)
-        val_loss = np.append(val_loss, av_val_loss)
+    plot.draw_loss([train_loss, train_loss_l, train_loss_c],
+                   [val_loss, val_loss_l, val_loss_c],
+                   ['Full', 'Localization', 'Classification'])
 
-        plot.draw_loss([train_loss, train_loss_l, train_loss_c],
-                       [val_loss, val_loss_l, val_loss_c],
-                       ['Full', 'Localization', 'Classification'])
-
-        if checkpoint_es(av_val_loss, ssd_net):
-            break
-    else:
-        if checkpoint_es(av_train_loss, ssd_net):
-            break
+    if checkpoint_es(av_val_loss, ssd_net):
+        break
 
 h5_train.close()
 h5_val.close()
