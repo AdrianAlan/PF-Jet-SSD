f42e8ef1d0ebe8c96d7c258c0b6b903263a45315
AdrianAlan <adrianalan.pol@gmail.com>
2020-04-30 11:10:54 -0400

    Change input quantization in special layers

--- ssd/layers/modules/qlayers.py
+++ ssd/layers/modules/qlayers.py
@@ -29,10 +29,11 @@ class BinaryConv2d(nn.Conv2d):
 
     def __init__(self, *kargs, **kwargs):
         super(BinaryConv2d, self).__init__(*kargs, **kwargs)
+        self.binarize_input = False
 
     def forward(self, input):
 
-        if input.size(1) != 2:
+        if self.binarize_input and input.size(1) != 2:
             input.data = Binary(input.data)
 
         if not hasattr(self.weight, 'org'):
@@ -54,16 +55,17 @@ class TernaryConv2d(nn.Conv2d):
 
     def __init__(self, *kargs, **kwargs):
         super(TernaryConv2d, self).__init__(*kargs, **kwargs)
+        self.binarize_input = False
 
     def forward(self, input):
 
-        # if input.size(1) != 2:
-        #     input.data = Binary(input.data)
+        if self.binarize_input and input.size(1) != 2:
+            input.data = Binary(input.data)
 
         if not hasattr(self.weight, 'org'):
             self.weight.org = self.weight.data.clone()
 
-        self.weight.data = Ternary(self.weight.org)
+        self.weight.data = Ternary(self.weight.org, delta=.5)
 
         out = nn.functional.conv2d(input, self.weight, None, self.stride,
                                    self.padding, self.dilation, self.groups)
@@ -76,8 +78,7 @@ class TernaryConv2d(nn.Conv2d):
 
 
 def Binary(tensor):
-    tensor_clone = tensor.clone()
-    return tensor_clone.sign()
+    return tensor.sign()
 
 
 def Ternary(tensor, delta=None):
