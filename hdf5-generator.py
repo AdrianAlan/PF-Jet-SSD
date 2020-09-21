e18eef9b58c791cf8402bf8dc5a9c748ee6a26bb
AdrianAlan <adrianalan.pol@gmail.com>
2020-09-21 10:27:38 +0200

    Repalce label generation from file name to particle information

--- hdf5-generator.py
+++ hdf5-generator.py
@@ -9,6 +9,7 @@ import uproot
 import warnings
 
 from collections import Counter
+from uproot_methods import PtEtaPhiMassLorentzVector, TLorentzVector
 from tqdm import tqdm
 
 
@@ -16,26 +17,17 @@ class PhysicsConstants():
 
     def __init__(self, example_file):
 
-        # Define quarks mass, in GeV
-        self.q_mass = {'b': 4.18, 'q': 0.096, 't': 173.1, 'W': 80.39,
-                       'h': 124.97}
+        # Define quarks mass (not used), in GeV
+        self.q_mass = {'b': 4.2, 'h': 125.0, 'q': 0.1, 't': 173.1, 'W': 80.4}
 
-        # Define radius of a jet
-        self.delta_r = {'b': 0.4, 'q': 0.4, 't': 0.8, 'W': 0.8, 'h': 0.8}
-
-        # Assigned jet classes
-        self.classes = {'b': 0, 'q': 3, 't': 2, 'W': 1, 'h': 1}
+        # Define jet constants
+        self.classes = {'b': 0, 'h': 1, 'q': 3, 't': 2, 'W': 1}
+        self.min_pt = {'b': 21., 'h': 312., 'q': 1., 't': 433., 'W': 201}
+        self.delta_r = .4
 
         self.example_file = example_file
         self.min_eta = -3
         self.max_eta = 3
-
-    def get_class(self, jtype):
-        return self.classes[jtype]
-
-    # Minimum detecable Pt
-    def get_minimum_pt(self, jtype):
-        return 2*self.q_mass[jtype] / self.delta_r[jtype]
 
     def get_edges_ecal(self, edge_index, tower, sample_events=1000):
         file = uproot.open(self.example_file)
@@ -63,7 +55,6 @@ class PhysicsConstants():
 class HDF5Generator:
 
     def __init__(self,
-                 jtype,
                  hdf5_dataset_path,
                  hdf5_dataset_size,
                  example_file,
@@ -71,8 +62,6 @@ class HDF5Generator:
 
         self.constants = PhysicsConstants(example_file)
 
-        self.jtype = jtype
-        self.minpt = self.constants.get_minimum_pt(jtype)
         self.edges_eta_ecal = self.constants.get_edges_ecal(edge_index=0,
                                                             tower='Tower.Eem')
         self.edges_phi_ecal = self.constants.get_edges_ecal(edge_index=2,
@@ -82,7 +71,6 @@ class HDF5Generator:
         self.edges_phi_hcal = self.constants.get_edges_ecal(edge_index=2,
                                                             tower='Tower.Ehad')
 
-        self.unique_label = self.constants.get_class(jtype)
         self.hdf5_dataset_path = hdf5_dataset_path
         self.hdf5_dataset_size = hdf5_dataset_size
         self.files_details = files_details
@@ -96,7 +84,7 @@ class HDF5Generator:
                 name='labels',
                 shape=(self.hdf5_dataset_size,),
                 maxshape=(None),
-                dtype=h5py.special_dtype(vlen=np.int16))
+                dtype=h5py.special_dtype(vlen=np.float32))
 
         hdf5_ecal_energy = hdf5_dataset.create_dataset(
                 name='ecal_energy',
@@ -150,27 +138,43 @@ class HDF5Generator:
             edges_full_file = towers['Tower.Edges[4]'].array()  # Crystal edge
 
             genjet = file['Delphes']['GenJet']
-            bbox_pt_full_file = genjet['GenJet.PT'].array()  # Jet PT
-            bbox_eta_full_file = genjet['GenJet.Eta'].array()  # Jet x
-            bbox_phi_full_file = genjet['GenJet.Phi'].array()  # Jet y
-            bbox_mass_full_file = genjet['GenJet.Mass'].array()  # Jet mass
+            genjet_pt_full_file = genjet['GenJet.PT'].array()  # Jet PT
+            genjet_eta_full_file = genjet['GenJet.Eta'].array()  # Jet x
+            genjet_phi_full_file = genjet['GenJet.Phi'].array()  # Jet y
+            genjet_mass_full_file = genjet['GenJet.Mass'].array()  # Jet mass
+
+            particle = file['Delphes']['Particle']
+            particle_pid_full_file = particle['Particle.PID'].array()
+            particle_px_full_file = particle['Particle.Px'].array()
+            particle_py_full_file = particle['Particle.Py'].array()
+            particle_pz_full_file = particle['Particle.Pz'].array()
+            particle_e_full_file = particle['Particle.E'].array()
 
             for event_number in np.arange(events[0], events[1], dtype=int):
 
                 # Get jet labels
-                bbox_etas = bbox_eta_full_file[event_number]
-                bbox_phis = bbox_phi_full_file[event_number]
+                jet_pt = genjet_pt_full_file[event_number]
+                jet_eta = genjet_eta_full_file[event_number]
+                jet_phi = genjet_phi_full_file[event_number]
+                jet_mass = genjet_mass_full_file[event_number]
 
-                etas_mask = ((bbox_etas > self.edges_eta_ecal[0]) &
-                             (bbox_etas < self.edges_eta_ecal[-1]))
+                etas_mask = ((jet_eta > self.edges_eta_ecal[0]) &
+                             (jet_eta < self.edges_eta_ecal[-1]))
 
-                bbox_etas = bbox_etas[etas_mask]
-                bbox_phis = bbox_phis[etas_mask]
-                bbox_pts = bbox_pt_full_file[event_number][etas_mask]
-                bbox_mass = bbox_mass_full_file[event_number][etas_mask]
+                jet_pt = jet_pt[etas_mask]
+                jet_eta = jet_eta[etas_mask]
+                jet_phi = jet_phi[etas_mask]
+                jet_mass = jet_mass[etas_mask]
 
-                labels = self.get_bboxes(bbox_pts, bbox_etas, bbox_phis,
-                                         bbox_mass)
+                particle_pid = particle_pid_full_file[event_number]
+                particle_px = particle_px_full_file[event_number]
+                particle_py = particle_py_full_file[event_number]
+                particle_pz = particle_pz_full_file[event_number]
+                particle_e = particle_e_full_file[event_number]
+
+                labels = self.get_labels(jet_pt, jet_eta, jet_phi, jet_mass,
+                                         particle_pid, particle_px,
+                                         particle_py, particle_pz, particle_e)
 
                 # Get ECAL info
                 ecal_mask = ecal_energy_full_file[event_number] > 0
@@ -224,17 +228,51 @@ class HDF5Generator:
 
         hdf5_dataset.close()
 
-    def get_bboxes(self, bbox_pts, bbox_etas, bbox_phis, bbox_mass):
+    def get_labels(self, j_pt, j_eta, j_phi, j_mass,
+                   p_pid, p_px, p_py, p_pz, p_e):
 
-        labels = []
+        plv = np.array([])
+        labels = np.empty((0, 5))
+        p_pid = np.abs(p_pid)
 
-        for pt, eta, phi, m in zip(bbox_pts, bbox_etas, bbox_phis, bbox_mass):
-            if pt > self.minpt:
+        for x, y, z, e in zip(p_px, p_py, p_pz, p_e):
+            plv = np.append(plv, TLorentzVector(x, y, z, e))
+
+        for pt, eta, phi, mass in zip(j_pt, j_eta, j_phi, j_mass):
+            label = None
+
+            if mass < 1.:
+                continue
+
+            jlv = PtEtaPhiMassLorentzVector(pt, eta, phi, mass)
+
+            # Order is important
+            if not label and pt > self.constants.min_pt['t']:
+                for lv in plv[p_pid == 6]:
+                    if lv.delta_r(jlv) < self.constants.delta_r:
+                        label = self.constants.classes['t']
+                        break
+            if not label and pt > self.constants.min_pt['W']:
+                for lv in plv[(p_pid == 23) | (p_pid == 24)]:
+                    if lv.delta_r(jlv) < self.constants.delta_r:
+                        label = self.constants.classes['W']
+                        break
+            if not label and pt > self.constants.min_pt['h']:
+                for lv in plv[p_pid == 25]:
+                    if lv.delta_r(jlv) < self.constants.delta_r:
+                        label = self.constants.classes['h']
+                        break
+            if not label and pt > self.constants.min_pt['b']:
+                for lv in plv[p_pid == 5]:
+                    if lv.delta_r(jlv) < self.constants.delta_r:
+                        label = self.constants.classes['b']
+                        break
+
+            if label is not None:
                 e = np.argmax(self.edges_eta_ecal >= eta) - 1
                 p = np.argmax(self.edges_phi_ecal >= phi) - 1
-                labels.append([self.unique_label, e, p, pt, m])
-
-        return np.asarray(labels)
+                labels = np.vstack((labels, [label, e, p, pt, mass]))
+        return labels
 
     def hcal_resize(self, indices_phi, indices_eta, energy):
         mask = (indices_eta >= 85) & (indices_eta <= 118)
@@ -388,17 +426,14 @@ if __name__ == '__main__':
         args.configuration,
         args.sourcedir)
 
-    jet_type = args.jtype[0]
-
     progress_bar = tqdm(total=total_events,
-                        desc=('Creating HDF5 Dataset for %s jets' % jet_type))
+                        desc=('Creating dataset for %s jets' % args.jtype))
 
     for index, file_dict in enumerate(files_details):
 
         dataset_size = int((index+1)*batch_size)-int((index)*batch_size)
 
         generator = HDF5Generator(
-                jtype=jet_type,
                 hdf5_dataset_path='%s/RSGraviton_%s_NARROW_%s-full.h5' %
                                   (args.savedir, args.jtype, index),
                 hdf5_dataset_size=dataset_size,
