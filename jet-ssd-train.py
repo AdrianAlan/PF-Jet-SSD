55e076bad86098557ce6b5d33c2ec81fe7043f93
AdrianAlan <adrianalan.pol@gmail.com>
2020-12-08 12:54:44 -0500

    Extend training time

--- jet-ssd-train.py
+++ jet-ssd-train.py
@@ -78,7 +78,7 @@ def execute(rank, world_size, name, quantized, dataset, output, training_pref,
                           momentum=training_pref['momentum'],
                           weight_decay=training_pref['weight_decay'])
     scheduler = optim.lr_scheduler.MultiStepLR(optimizer,
-                                               milestones=[20, 30, 40, 45],
+                                               milestones=[40, 60, 80, 90],
                                                gamma=0.5)
     if rank == 0:
         cp_es = EarlyStopping(patience=training_pref['patience'],
