1f12443727f9f294ea5a8994973ec89b1debffef
AdrianAlan <adrianalan.pol@gmail.com>
2021-02-05 17:49:58 +0100

    Update generator to match h5 dataset changes

--- ssd/generator.py
+++ ssd/generator.py
@@ -1,5 +1,6 @@
 import h5py
 import torch
+import torch.cuda as tcuda
 
 from ssd import qutils
 
@@ -24,31 +25,39 @@ class CalorimeterJetDataset(torch.utils.data.Dataset):
         if not hasattr(self, 'hdf5_dataset'):
             self.open_hdf5()
 
-        # Load calorimeter
-        indices_ecal_phi = torch.cuda.LongTensor([self.ecal_phi[index]],
-                                                 device=self.rank)
-        indices_ecal_eta = torch.cuda.LongTensor([self.ecal_eta[index]],
-                                                 device=self.rank)
-        ecal_energy = torch.cuda.FloatTensor(self.ecal_energy[index],
+        idx_eFTrack_Eta = tcuda.LongTensor([self.eFTrack_Eta[index]],
+                                           device=self.rank)
+        idx_eFTrack_Phi = tcuda.LongTensor([self.eFTrack_Phi[index]],
+                                           device=self.rank)
+        val_eFTrack_PT = tcuda.FloatTensor(self.eFTrack_PT[index],
+                                           device=self.rank)
+
+        idx_eFPhoton_Eta = tcuda.LongTensor([self.eFPhoton_Eta[index]],
+                                            device=self.rank)
+        idx_eFPhoton_Phi = tcuda.LongTensor([self.eFPhoton_Phi[index]],
+                                            device=self.rank)
+        val_eFPhoton_ET = tcuda.FloatTensor(self.eFPhoton_ET[index],
+                                            device=self.rank)
+
+        idx_eFNHadron_Eta = tcuda.LongTensor([self.eFNHadron_Eta[index]],
+                                             device=self.rank)
+        idx_eFNHadron_Phi = tcuda.LongTensor([self.eFNHadron_Phi[index]],
+                                             device=self.rank)
+        val_eFNHadron_ET = tcuda.FloatTensor(self.eFNHadron_ET[index],
                                              device=self.rank)
 
-        indices_hcal_phi = torch.cuda.LongTensor([self.hcal_phi[index]],
-                                                 device=self.rank)
-        indices_hcal_eta = torch.cuda.LongTensor([self.hcal_eta[index]],
-                                                 device=self.rank)
-        hcal_energy = torch.cuda.FloatTensor(self.hcal_energy[index],
-                                             device=self.rank)
-
-        calorimeter, scaler = self.process_images(indices_ecal_phi,
-                                                  indices_hcal_phi,
-                                                  indices_ecal_eta,
-                                                  indices_hcal_eta,
-                                                  ecal_energy,
-                                                  hcal_energy)
+        calorimeter, scaler = self.process_images(idx_eFTrack_Eta,
+                                                  idx_eFPhoton_Eta,
+                                                  idx_eFNHadron_Eta,
+                                                  idx_eFTrack_Phi,
+                                                  idx_eFPhoton_Phi,
+                                                  idx_eFNHadron_Phi,
+                                                  val_eFTrack_PT,
+                                                  val_eFPhoton_ET,
+                                                  val_eFNHadron_ET)
 
         # Set labels
-        labels_raw = torch.cuda.FloatTensor(self.labels[index],
-                                            device=self.rank)
+        labels_raw = tcuda.FloatTensor(self.labels[index], device=self.rank)
         labels_processed = self.process_labels(labels_raw, scaler)
 
         return calorimeter, labels_processed
@@ -63,34 +72,47 @@ class CalorimeterJetDataset(torch.utils.data.Dataset):
     def open_hdf5(self):
         self.hdf5_dataset = h5py.File(self.source, 'r')
 
-        self.ecal_energy = self.hdf5_dataset['ecal_energy']
-        self.ecal_phi = self.hdf5_dataset['ecal_phi']
-        self.ecal_eta = self.hdf5_dataset['ecal_eta']
+        self.eFTrack_Eta = self.hdf5_dataset['EFlowTrack_Eta']
+        self.eFTrack_Phi = self.hdf5_dataset['EFlowTrack_Phi']
+        self.eFTrack_PT = self.hdf5_dataset['EFlowTrack_PT']
 
-        self.hcal_energy = self.hdf5_dataset['hcal_energy']
-        self.hcal_phi = self.hdf5_dataset['hcal_phi']
-        self.hcal_eta = self.hdf5_dataset['hcal_eta']
+        self.eFPhoton_Eta = self.hdf5_dataset['EFlowPhoton_Eta']
+        self.eFPhoton_Phi = self.hdf5_dataset['EFlowPhoton_Phi']
+        self.eFPhoton_ET = self.hdf5_dataset['EFlowPhoton_ET']
+
+        self.eFNHadron_Eta = self.hdf5_dataset['EFlowNeutralHadron_Eta']
+        self.eFNHadron_Phi = self.hdf5_dataset['EFlowNeutralHadron_Phi']
+        self.eFNHadron_ET = self.hdf5_dataset['EFlowNeutralHadron_ET']
 
         self.labels = self.hdf5_dataset['labels']
 
         self.dataset_size = len(self.labels)
 
     def process_images(self,
-                       indices_ecal_phi, indices_hcal_phi,
-                       indices_ecal_eta, indices_hcal_eta,
-                       ecal_energy, hcal_energy):
+                       idx_eFTrack_Eta, idx_eFPhoton_Eta, idx_eFNHadron_Eta,
+                       idx_eFTrack_Phi, idx_eFPhoton_Phi, idx_eFNHadron_Phi,
+                       val_eFTrack_PT, val_eFPhoton_ET, val_eFNHadron_ET):
 
-        c_ecal = torch.zeros(indices_ecal_phi.size(1),
-                             dtype=torch.long).cuda(self.rank)
-        c_hcal = torch.ones(indices_hcal_phi.size(1),
-                            dtype=torch.long).cuda(self.rank)
-        indices_channels = torch.unsqueeze(torch.cat((c_ecal, c_hcal)), 0)
-        indices_phi = torch.cat((indices_ecal_phi, indices_hcal_phi), 1)
-        indices_eta = torch.cat((indices_ecal_eta, indices_hcal_eta), 1)
-        energy = torch.cat((ecal_energy, hcal_energy))
-        scaler = torch.max(energy) / 10
+        v0 = 0*torch.ones(idx_eFTrack_Eta.size(1),
+                          dtype=torch.long).cuda(self.rank)
+        v1 = 1*torch.ones(idx_eFPhoton_Eta.size(1),
+                          dtype=torch.long).cuda(self.rank)
+        v2 = 2*torch.ones(idx_eFNHadron_Eta.size(1),
+                          dtype=torch.long).cuda(self.rank)
 
-        i = torch.cat((indices_channels, indices_eta, indices_phi), 0)
+        idx_channels = torch.cat((v0, v1, v2)).unsqueeze(0)
+        idx_eta = torch.cat((idx_eFTrack_Eta,
+                             idx_eFPhoton_Eta,
+                             idx_eFNHadron_Eta), 1)
+        idx_phi = torch.cat((idx_eFTrack_Phi,
+                             idx_eFPhoton_Phi,
+                             idx_eFNHadron_Phi), 1)
+        energy = torch.cat((val_eFTrack_PT,
+                            val_eFPhoton_ET,
+                            val_eFNHadron_ET))
+        scaler = torch.max(energy)
+
+        i = torch.cat((idx_channels, idx_eta, idx_phi), 0)
         v = energy / scaler
 
         if self.qbits is not None:
@@ -101,8 +123,8 @@ class CalorimeterJetDataset(torch.utils.data.Dataset):
                                                             self.height]))
         return pixels.to_dense(), scaler
 
-    def process_labels(self, labels_raw, scaler_mass):
-        labels_reshaped = labels_raw.reshape(-1, 5)
+    def process_labels(self, labels_raw, scaler):
+        labels_reshaped = labels_raw.reshape(-1, 4)
         labels = torch.empty_like(labels_reshaped)
 
         # Set fractional coordinates
@@ -112,11 +134,7 @@ class CalorimeterJetDataset(torch.utils.data.Dataset):
         labels[:, 3] = (labels_reshaped[:, 2] + self.size) / float(self.height)
 
         # Set class label
-        labels[:, 4] = labels_reshaped[:, 0]
-
-        # Concatinate auxilary labels
-        labels_reshaped[:, 4] = labels_reshaped[:, 4] / scaler_mass
-        labels = torch.cat((labels, labels_reshaped[:, 4].unsqueeze(1)), 1)
+        labels = torch.cat((labels, labels_reshaped[:, 0].unsqueeze(1) + 1), 1)
 
         if self.return_pt:
             labels = torch.cat((labels, labels_reshaped[:, 3].unsqueeze(1)), 1)
