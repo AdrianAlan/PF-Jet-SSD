ef12100f13345bc6e0dfa8d5295e2d98f62f5625
Adrian Alan Pol <adrianalan.pol@gmail.com>
2021-07-05 15:32:29 +0200

    Add new measurements

--- jet-ssd-inference.py
+++ jet-ssd-inference.py
@@ -18,18 +18,18 @@ if __name__ == '__main__':
     latency_pytorch_fp32 = [158.69, 254.08, 472.38, 1009.10, 9352.68, np.nan, np.nan, np.nan, np.nan]
     latency_pytorch_int8 = [40.61, 61.07, 129.62, 269.42, 648.28, 1346.26, 2728.85, 5437.04, 10635.50]
     latency_onnx_runtime_fp32 = [110.59, 234.71, 486.53, 994.54, 2003.04, 3979.58,  7965.95, 17490.42, np.nan]
-    latency_onnx_runtime_int8 = [147.63, 303.70, 613.34, 1252.22, 2511.32, 5024.10, 10013.98, 22700.14, np.nan]
-    latency_tensorrt_fp32 = [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
-    latency_tensorrt_fp16 = [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
-    latency_tensorrt_int8 = [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
+    latency_onnx_runtime_int8 = [109.25, 225.58, 446.04, 890.54, 1790.03, 3604.68, 7151.72, 17162.30, np.nan]
+    latency_tensorrt_fp32 = [3.95, 7.42, 14.02, 27.12, 50.85, 100.78, 204.81, np.nan, np.nan]
+    latency_tensorrt_fp16 = [1.62, 3.08, 5.92, 11.59, 22.95, 46.18, 92.66, 187.39, np.nan]
+    latency_tensorrt_int8 = [1.21, 2.19, 4.00, 7.59, 14.87, 29.30, 59.33, np.nan, np.nan]
 
     throughput_pytorch_fp32 = [6.30, 7.87, 8.47, 7.93, 1.71, np.nan, np.nan, np.nan, np.nan]
     throughput_pytorch_int8 = [24.62, 32.75, 30.86, 29.69, 24.68, 23.77, 23.45, 23.54, 24.07]
     throughput_onnx_runtime_fp32 = [9.04, 8.52, 8.22, 8.04, 7.99, 8.04, 8.03, 7.32, np.nan]
-    throughput_onnx_runtime_int8 = [6.77, 6.59, 6.52, 6.39, 6.37, 6.37, 6.39, 5.64, np.nan]
-    throughput_tensorrt_fp32 = [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
-    throughput_tensorrt_fp16 = [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
-    throughput_tensorrt_int8 = [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
+    throughput_onnx_runtime_int8 = [9.15, 8.87, 8.97, 8.98, 8.94, 8.88, 8.95, 7.46, np.nan]
+    throughput_tensorrt_fp32 = [252.91, 269.47, 285.40, 295.02, 314.66, 317.53, 312.48, np.nan, np.nan]
+    throughput_tensorrt_fp16 = [615.70, 650.16, 675.94, 690.13, 697.18, 693.01, 690.72, 683.05, np.nan]
+    throughput_tensorrt_int8 = [823.84, 913.18, 999.69, 1053.53, 1076.07, 1092.17, 1078.69, np.nan, np.nan]
 
     plot = Plotting(save_dir=config['output']['plots'])
 
