b451bbe3f827c1d2302ead43f6d6255afa9945fa
Adrian Alan Pol <adrianalan.pol@gmail.com>
2021-07-13 15:32:29 +0200

    Refactor MultBox loss

--- ssd/layers/modules/multibox_loss.py
+++ ssd/layers/modules/multibox_loss.py
@@ -27,8 +27,8 @@ class MultiBoxLoss(nn.Module):
         self.beta_loc = 1.0
         self.beta_cnf = 1.0
         self.beta_reg = 1.0
-        self.rank = rank
-        self.priors = priors
+        self.device = torch.device(rank)
+        self.defaults = priors.data
         self.n_classes = n_classes
         self.threshold = min_overlap
         self.negpos_ratio = neg_pos
@@ -50,47 +50,49 @@ class MultiBoxLoss(nn.Module):
             loss_c: classification loss
             loss_r: regression loss
         """
-        loc_data, conf_data, regr_data = predictions
-        defaults = self.priors[:loc_data.size(1), :].data
+        loc_data, cnf_data, reg_data = predictions
 
         bs = loc_data.size(0)  # batch size
-        n_priors = self.priors.size(0)  # number of priors
+        n_priors = self.defaults.size(0)  # number of priors
 
         # Match priors with ground truth boxes
-        loc_t = torch.Tensor(bs, n_priors, 2)
-        conf_t = torch.LongTensor(bs, n_priors)
-        regr_t = torch.Tensor(bs, n_priors, 1)
-        for idx in range(bs):
-            coords = targets[idx][:, :4].data  # truth coordinates
-            labels = targets[idx][:, 4].data  # truth labels
-            regres = targets[idx][:, -1:].data  # truth auxiliary regression
-            match(self.threshold, coords, defaults, self.variance, labels,
-                  regres, loc_t, conf_t, regr_t, idx)
-        if self.rank != 'cpu':
-            loc_t = loc_t.cuda(self.rank)
-            conf_t = conf_t.cuda(self.rank)
-            regr_t = regr_t.cuda(self.rank)
-        else:
-            loc_t = loc_t.cpu()
-            conf_t = conf_t.cpu()
-            regr_t = regr_t.cpu()
-        loc_t = Variable(loc_t, requires_grad=False)
-        conf_t = Variable(conf_t, requires_grad=False)
-        regr_t = Variable(regr_t, requires_grad=False)
+        loc_truth = torch.Tensor(bs, n_priors, 2).to(self.device)
+        cnf_truth = torch.LongTensor(bs, n_priors).to(self.device)
+        reg_truth = torch.Tensor(bs, n_priors, 1).to(self.device)
+
+        for i in range(bs):
+            # Target
+            target_coords = targets[i][:, :4].data  # Target coordinates
+            target_labels = targets[i][:, 4].data  # Target labels
+            target_regres = targets[i][:, -1:].data  # Target regression
+            match(self.threshold,
+                  target_coords,
+                  self.defaults,
+                  self.variance,
+                  target_labels,
+                  target_regres,
+                  loc_truth,
+                  cnf_truth,
+                  reg_truth,
+                  i)
+
+        loc_truth = Variable(loc_truth, requires_grad=False)
+        cnf_truth = Variable(cnf_truth, requires_grad=False)
+        reg_truth = Variable(reg_truth, requires_grad=False)
 
         # Mask confidence
-        pos = conf_t > 0
+        pos = cnf_truth > 0
         num_pos = pos.sum(dim=1, keepdim=True)
 
         # Localization Loss (Smooth L1)
-        pos_idx = pos.unsqueeze(pos.dim()).expand_as(loc_t)
-        loc_p = loc_data[pos_idx].view(-1, 2)
-        loc_t = loc_t[pos_idx].view(-1, 2)
-        loss_l = F.smooth_l1_loss(loc_p, loc_t, reduction='sum')
+        pos_idx = pos.unsqueeze(pos.dim()).expand_as(loc_truth)
+        loc_prediction = loc_data[pos_idx].view(-1, 2)
+        loc_truth = loc_truth[pos_idx].view(-1, 2)
+        loss_l = F.smooth_l1_loss(loc_prediction, loc_truth, reduction='sum')
 
         # Compute max conf across batch for hard negative mining
-        b_conf = conf_data.view(-1, self.n_classes)
-        loss_c = log_sum_exp(b_conf) - b_conf.gather(1, conf_t.view(-1, 1))
+        b_conf = cnf_data.view(-1, self.n_classes)
+        loss_c = log_sum_exp(b_conf) - b_conf.gather(1, cnf_truth.view(-1, 1))
         loss_c = loss_c.view(bs, -1)
         loss_c[pos] = 0  # filter out pos boxes
         _, loss_idx = loss_c.sort(1, descending=True)
@@ -100,17 +102,18 @@ class MultiBoxLoss(nn.Module):
         neg = idx_rank < num_neg.expand_as(idx_rank)
 
         # Confidence Loss Including Positive and Negative Examples
-        pos_idx = pos.unsqueeze(2).expand_as(conf_data)
-        neg_idx = neg.unsqueeze(2).expand_as(conf_data)
-        conf_p = conf_data[(pos_idx+neg_idx).gt(0)].view(-1, self.n_classes)
-        targets_weighted = conf_t[(pos+neg).gt(0)]
-        loss_c = F.cross_entropy(conf_p, targets_weighted, reduction='sum')
+        pos_idx = pos.unsqueeze(2).expand_as(cnf_data)
+        neg_idx = neg.unsqueeze(2).expand_as(cnf_data)
+        cnf_idx = (pos_idx+neg_idx).gt(0)
+        trt_idx = (pos+neg).gt(0)
+        cnf_prediction = cnf_data[cnf_idx].view(-1, self.n_classes)
+        loss_c = F.binary_cross_entropy(cnf_prediction, cnf_truth[trt_idx], reduction='sum')
 
         # Compute regression loss
-        pos_idx = pos.unsqueeze(pos.dim()).expand_as(regr_data)
-        regr_p = regr_data[pos_idx].view(-1, 1)
-        regr_t = regr_t[pos_idx].view(-1, 1)
-        loss_r = F.l1_loss(regr_p, regr_t, reduction='sum')
+        pos_idx = pos.unsqueeze(pos.dim()).expand_as(reg_data)
+        reg_prediction = reg_data[pos_idx].view(-1, 1)
+        reg_truth = reg_truth[pos_idx].view(-1, 1)
+        loss_r = F.l1_loss(reg_prediction, reg_truth, reduction='sum')
 
         # Final normalized losses
         N = num_pos.data.sum().float()
